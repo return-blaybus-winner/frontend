350e4fd7189a373ce52c9a9d2c425ebfb315c7e4
Byeongmin Jeon <qudals7613@gmail.com>
2025-08-23 17:45:17 +0900

    feat: 카테고리 그리드 컴포넌트에서 모의 데이터 대신 API를 사용하여 카테고리 목록 업데이트

--- src/app/(with-gnb)/_components/category-grid.tsx
+++ src/app/(with-gnb)/_components/category-grid.tsx
@@ -1,25 +1,28 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
-import { CATEGORY_ITEMS } from "../_constants/mock-data";
+import { useProjectCategories } from "@/app/_hooks/use-project-categories";
 
 export default function CategoryGrid() {
+  const { data: categories } = useProjectCategories();
   return (
     <div className="mx-auto flex items-center">
-      {CATEGORY_ITEMS.map((item) => (
-        <Link 
-          href={item.href} 
-          className="px-4 flex flex-col gap-1" 
-          key={item.id}
+      {categories?.map((item, index) => (
+        <Link
+          href={`/projects/?category=${item.code}`}
+          className="px-4 flex flex-col gap-1"
+          key={item.code}
         >
           <div className="relative h-[100px] w-[124px] flex items-center justify-center">
             <Image
-              src={item.imageUrl}
-              alt="Exhibition Planning"
+              src={`/images/home/${index}.png`}
+              alt="Project Category"
               className="rounded-md object-contain absolute py-[22px]"
               fill
             />
           </div>
-          <div className="text-base text-center">{item.label}</div>
+          <div className="text-base text-center">{item.description}</div>
         </Link>
       ))}
     </div>
