b7dc3e49fa89c753c41753f5251a9ef561e4d453
Byeongmin Jeon <qudals7613@gmail.com>
2025-08-24 15:07:46 +0900

    feat: update redirect url

---
+++ src/app/api/auth/kakao/success/route.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const accessToken = searchParams.get("token");
+
+  if (accessToken) {
+    const response = NextResponse.redirect(new URL("/signup", request.url));
+
+    response.cookies.set("accessToken", accessToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      maxAge: 60 * 60 * 24 * 7, // 7일
+      path: "/",
+    });
+
+    return response;
+  }
+
+  return NextResponse.redirect(new URL(request.url));
+}
